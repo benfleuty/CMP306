277f991df1239276aa47a7ab2a19e5506ee196dd
Ben Fleuty <bfleuty@outlook.com>
2021-09-28 13:34:37 +0100

    Add block work and report links

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="refresh" content="2"/>
+<!--    <meta http-equiv="refresh" content="2"/>-->
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
@@ -21,10 +21,10 @@
             <div class="card-body">
                 <h5 class="card-title">Coursework</h5>
                 <ul class="card-text list-no-decoration">
-                    <li><a href="coursework-block-one" class="card-link">Block One</a></li>
-                    <li><a href="coursework-block-two" class="card-link">Block Two</a></li>
-                    <li><a href="coursework-block-three" class="card-link">Block Three</a></li>
-                    <li><a href="coursework-block-four" class="card-link">Block Four</a></li>
+                    <li><a href="coursework-block-one" class="card-link">Block One Work</a> | <a href="coursework-block-one-report">Report</a> </li>
+                    <li><a href="coursework-block-two" class="card-link">Block Two Work</a> | <a href="coursework-block-two-report">Report</a> </li>
+                    <li><a href="coursework-block-three" class="card-link">Block Three Work</a> | <a href="coursework-block-three-report">Report</a> </li>
+                    <li><a href="coursework-block-four" class="card-link">Block Four Work</a> | <a href="coursework-block-four-report">Report</a> </li>
 
                 </ul>
                 <p class="card-text"><small class="text-muted">Last updated xxx ago</small></p>
