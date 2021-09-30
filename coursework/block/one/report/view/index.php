833eec4fcfa5a0381aca79935d21015170784f47
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-09-30 13:39:45 +0100

    add mvc structure to all pages

---
+++ coursework/block/one/report/view/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Block One Report</title>
+    <link rel="stylesheet" href="../../../../../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="../../../../../assets/css/Article-Clean.css">
+    <link rel="stylesheet" href="../../../../../assets/css/Article-Dual-Column.css">
+    <link rel="stylesheet" href="../../../../../assets/css/Article-List.css">
+    <link rel="stylesheet" href="../../../../../assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="../../../../../assets/css/Header-Blue.css">
+    <link rel="stylesheet" href="../../../../../assets/css/Header-Dark.css">
+    <link rel="stylesheet" href="../../../../../assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="../../../../../assets/css/Projects-Horizontal.css">
+    <link rel="stylesheet" href="../../../../../assets/css/Team-Boxed.css">
+</head>
+
+<body>
+<header class="header-blue">
+    <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
+        <div class="container-fluid"><a class="navbar-brand" href="#"><strong>Ben Fleuty |&nbsp;CMP
+                    306</strong><br><strong>Dynamic Web Development</strong><br></a></div>
+    </nav>
+    <div class="container hero">
+        <div class="row">
+            <div class="col-12 col-lg-6 col-xl-5 offset-xl-1">
+                <h1>Block One Report</h1><a class="btn btn-light btn-lg action-button" role="button" href="../..">View
+                    the work</a>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="article-clean">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2">
+                <div class="intro">
+                    <h1 class="text-center">Relational Databases</h1>
+                    <p class="text-center"><span class="by">by</span> <a href="#">Ben Fleuty</a><span class="date">WC 2021/09/27</span>
+                    </p>
+                </div>
+                <div class="text">
+                    <h2>Entity Relationship Diagrams</h2>
+                    <p>Below are the ERDs for this week's work.</p>
+                    <figure class="figure d-block">
+                        <img class="figure-img" src="../image/ERD.svg" alt="ERD">
+                        <figcaption class="figure-caption">Fig. 1</figcaption>
+                    </figure>
+                    <figure class="figure d-block">
+                        <img class="figure-img" src="../image/ERD_Simple.svg" alt="ERD Simple">
+                        <figcaption class="figure-caption">Fig. 2</figcaption>
+                    </figure>
+                    <p>Above are two diagrams for the same table. Fig. 1 shows the tables and their respective fields.
+                        This is okay in a small table like this but this is not a scalable design. Fig. 2 shows the same
+                        tables but without their attributes. This is a more scalable design since the tables and their
+                        relations are still shown, but in a more compact and clutter-free way. This is better for large
+                        databases as the structure of the database can easily be seen.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="footer-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 item text">
+                <h3>Ben Fleuty | 1900040</h3>
+                <p>This website is for educational purposes only. Content may be subject to copyright.</p>
+            </div>
+        </div>
+        <p class="copyright">Ben Fleuty and whoever owns the stuff I'm... borrowing © 2021</p>
+    </div>
+</footer>
+<script src="../../../../../assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ view/index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>CMP 306 Home | 1900040</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="../assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="../assets/css/Header-Blue.css">
+    <link rel="stylesheet" href="../assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="../assets/css/Projects-Horizontal.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+<body>
+<header class="header-blue">
+    <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
+        <div class="container-fluid"><a class="navbar-brand" href="#">Ben Fleuty |&nbsp;CMP 306<br>Dynamic Web
+                Development</a>
+            <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span
+                        class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false"
+                                                     data-bs-toggle="dropdown">Block One</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="../coursework/block/one">Work</a>
+                            <a class="dropdown-item" href="../coursework/block/one/report">Report</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container hero">
+        <div class="row">
+            <div class="col-12 col-lg-6 col-xl-5 offset-xl-1">
+                <h1>CMP 306 Dynamic Web Development 2 | 2021 - 2022</h1>
+                <p>Here you will find all my work for this module. The block work and their reports can be found in the
+                    above navigation bar.<br>Any weekly work can be found below.</p>
+                <button class="btn btn-light btn-lg action-button" type="button">View the blocks</button>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="projects-horizontal">
+    <div class="container">
+        <div class="intro">
+            <h2 class="text-center">Weekly Work</h2>
+            <p class="text-center">Here you will find links to the work completed each week if it is separate to the
+                block work.</p>
+        </div>
+        <div class="row projects">
+            <div class="col-sm-6 item">
+                <div class="row">
+                    <div class="col-md-12 col-lg-5"><a href="../week-one"><img class="img-fluid"
+                                                                               src="../assets/img/desk.jpg" alt="plant on a desk"></a>
+                    </div>
+                    <div class="col">
+                        <h3 class="name">Week 1</h3>
+                        <p class="description">This is my interest.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="footer-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 item text">
+                <h3>Ben Fleuty | 1900040</h3>
+                <p>This website is for educational purposes only. Content may be subject to copyright.</p>
+            </div>
+        </div>
+        <p class="copyright">Ben Fleuty and whoever owns the stuff I'm... borrowing © 2021</p>
+    </div>
+</footer>
+<script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
