6479b582286ebed8ef99902df187260bd1262726
Ben Fleuty <bfleuty@outlook.com>
2021-09-30 12:03:22 +0100

    make pretty

--- index.php
+++ index.php
@@ -2,63 +2,80 @@
 <html lang="en">
 <head>
     <title>CMP 306 Home | 1900040</title>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!--    <meta http-equiv="refresh" content="2"/>-->
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="assets/css/Header-Blue.css">
+    <link rel="stylesheet" href="assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="assets/css/Projects-Horizontal.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
-<div id="header">
-    <h1>CMP 306 | Dynamic Web Development</h1>
-    <h2>Ben Fleuty | 1900040</h2>
-</div>
-<div id="mainContent">
-    <div class="card-deck">
-        <div class="card">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Coursework</h5>
-                <ul class="card-text list-no-decoration">
-                    <li><a href="coursework-block-one" class="card-link">Block One Work</a> | <a href="coursework-block-one-report">Report</a> </li>
-                    <li><a href="coursework-block-two" class="card-link">Block Two Work</a> | <a href="coursework-block-two-report">Report</a> </li>
-                    <li><a href="coursework-block-three" class="card-link">Block Three Work</a> | <a href="coursework-block-three-report">Report</a> </li>
-                    <li><a href="coursework-block-four" class="card-link">Block Four Work</a> | <a href="coursework-block-four-report">Report</a> </li>
-
+<header class="header-blue">
+    <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
+        <div class="container-fluid"><a class="navbar-brand" href="#">Ben Fleuty |&nbsp;CMP 306<br>Dynamic Web
+                Development</a>
+            <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span
+                        class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false"
+                                                     data-bs-toggle="dropdown" href="coursework/block/one">Block One</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="coursework/block/one/">Work</a>
+                            <a class="dropdown-item" href="coursework/block/one/report">Report</a>
+                        </div>
+                    </li>
                 </ul>
-                <p class="card-text"><small class="text-muted">Last updated xxx ago</small></p>
             </div>
         </div>
-        <div class="card">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Week One</h5>
-                <p class="card-text">View the work I have completed for week one here.</p>
-                <p class="card-text"><small class="text-muted">Last updated xxx mins ago</small></p>
-            </div>
-            <div class="card-footer">
-                <a href="./week-one" class="btn btn-primary">Open</a>
-            </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Week Two</h5>
-                <p class="card-text">View the work I have completed for week two here.</p>
-                <p class="card-text"><small class="text-muted">Last updated xxx mins ago</small></p>
-            </div>
-            <div class="card-footer">
-                <a href="./week-two" class="btn btn-primary">Open</a>
+    </nav>
+    <div class="container hero">
+        <div class="row">
+            <div class="col-12 col-lg-6 col-xl-5 offset-xl-1">
+                <h1>CMP 306 Dynamic Web Development 2 | 2021 - 2022</h1>
+                <p>Here you will find all my work for this module. The block work and their reports can be found in the
+                    above navigation bar.<br>Any weekly work can be found below.</p>
+                <button class="btn btn-light btn-lg action-button" type="button">View the blocks</button>
             </div>
         </div>
     </div>
-</div>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</header>
+<section class="projects-horizontal">
+    <div class="container">
+        <div class="intro">
+            <h2 class="text-center">Weekly Work</h2>
+            <p class="text-center">Here you will find links to the work completed each week if it is separate to the
+                block work.</p>
+        </div>
+        <div class="row projects">
+            <div class="col-sm-6 item">
+                <div class="row">
+                    <div class="col-md-12 col-lg-5"><a href="week-one"><img class="img-fluid" src="assets/img/desk.jpg"></a>
+                    </div>
+                    <div class="col">
+                        <h3 class="name">Week 1</h3>
+                        <p class="description">This is my interest.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="footer-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 item text">
+                <h3>Ben Fleuty | 1900040</h3>
+                <p>This website is for educational purposes only. Content may be subject to copyright.</p>
+            </div>
+        </div>
+        <p class="copyright">Ben Fleuty and whoever owns the stuff I'm... borrowing © 2021</p>
+    </div>
+</footer>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
