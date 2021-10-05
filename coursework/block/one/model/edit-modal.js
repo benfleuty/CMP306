629403744252988e3898f5d5c5b7422b54d3cfdf
Ben Fleuty <bfleuty@outlook.com>
2021-10-06 00:19:54 +0100

    Fix UTF8 JSON encoding issue
    Added ignore flag which fixed everything. GIGO!

--- coursework/block/one/model/edit-modal.js
+++ coursework/block/one/model/edit-modal.js
@@ -1,16 +1,19 @@
 $(function () {
-    $('.plant-modal-button').on('click',function (e){
-        let plant_id = e.target.id - 1;
+    $('.plant-modal-button').on('click', function (e) {
+        let plant_id = e.target.id ;
+console .log(plant_id);
+        $.ajax({
+            type: "POST",
+            url: '../model/get-selected-plant-desc.php',
+            data: {plant_id: plant_id},
+            success: function (response) {
+                var jsonData = JSON.parse(response);
+                let title = jsonData.common_name + " (" + jsonData.scientific_name + ")";
+                let desc = jsonData.description;
 
-        let out = "";
-
-        $.ajax{
-
-        }
-
-
-        $('#plantModal .modal-title').html(out);
-        console.log(out);
-
+                $("#plantModal .modal-title").html(title);
+                $("#plantModal .modal-body").html(desc);
+            }
+        })
     })
 })
--- coursework/block/one/model/get-selected-plant-desc.php
+++ coursework/block/one/model/get-selected-plant-desc.php
@@ -1,1 +1,29 @@
 <?php
+include "../model/connection.php";
+global $conn;
+
+if (!isset($_POST["plant_id"]) || empty($_POST["plant_id"])) {
+    //error
+    die();
+}
+
+$sql = "SELECT * FROM CMP306BlockOnePlants WHERE id = " . $_POST["plant_id"] ;
+$res = mysqli_query($conn, $sql)->fetch_assoc();
+
+$id = $res["id"];
+$scientific_name = $res["scientific_name"];
+$common_name = $res["common_name"];
+$keep_location = $res["keep_location"];
+$description = $res["description"];
+$image = $res["image"];
+
+
+$data = array('id' => $id,
+    'scientific_name' => $scientific_name,
+    'common_name' => $common_name,
+    'keep_location' => $keep_location,
+    'description' => $description,
+    'image' => $image
+);
+
+echo json_encode($data,JSON_INVALID_UTF8_IGNORE);
