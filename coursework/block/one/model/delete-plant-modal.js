9614f67b416a9a8c625dfbb84ce07a8f191f28eb
Ben Fleuty <bfleuty@outlook.com>
2021-10-09 20:32:07 +0100

    Implement plant deletion
    The modal will close regardless of the state of the response.
    The result is currently logged to console.

--- coursework/block/one/model/delete-plant-modal.js
+++ coursework/block/one/model/delete-plant-modal.js
@@ -1,5 +1,26 @@
 $(function () {
-    $("#plantModal").on("click","#btnDeletePlant", function () {
-        alert("Plant 'deleted'");
+    $("#plantModal").on("click", "#btnDeletePlant", function () {
+        let complete = false;
+
+        $.ajax({
+            type: "POST",
+            url: '../model/delete-plant.php',
+            success: function (response) {
+                var jsonData = JSON.parse(response);
+                if (jsonData.status === 'success') complete = true;
+
+                $("#plantModal").modal("hide");
+
+                if(!complete){
+                    // error message
+                    console.error("Could not delete plant!");
+                    return;
+                }
+                // Otherwise successful deletion
+                console.log("Plant successfully deleted");
+            }
+        })
+
+
     })
 })
