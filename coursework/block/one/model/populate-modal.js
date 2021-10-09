a337980bc4ba571c6b9466c52bce970e4e755cba
Ben Fleuty <bfleuty@outlook.com>
2021-10-09 19:18:01 +0100

    Add ability to store current plant ID for CRUD ops

--- coursework/block/one/model/populate-modal.js
+++ coursework/block/one/model/populate-modal.js
@@ -1,14 +1,16 @@
 $(function () {
-    $('.plant-modal-button').on('click',function (e) {
-        let plant_id = e.target.id ;
+    $('.plant-modal-button').on('click', function (e) {
+        let pid = e.target.id;
         $.ajax({
             type: "POST",
             url: '../model/get-selected-plant-desc.php',
-            data: {plant_id: plant_id},
+            data: {plant_id: pid},
             success: function (response) {
                 var jsonData = JSON.parse(response);
                 let title = jsonData.common_name + " (" + jsonData.scientific_name + ")";
                 let desc = jsonData.description;
+                let plantId = jsonData.plant_id;
+                title += '<span id="plantID" style="display: none">' + plantId + '</span>';
 
                 $("#plantModal .modal-title").html(title);
                 $("#plantModal .modal-body").html(desc);
@@ -17,7 +19,7 @@ $(function () {
 
         let x = '<button type="button" id="btnDeletePlant" class="btn btn-danger modal-button">Delete</button>' +
             '<button type="button" id="btnEditPlant" class="btn btn-warning modal-button">Edit</button>';
-       $("#plantModal .modal-footer").html(x);
+        $("#plantModal .modal-footer").html(x);
     })
 
 
