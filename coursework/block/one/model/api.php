786532a5ab3f166c45bd49272185be71766bb715
= <=>
2021-10-10 22:03:40 +0100

    MVCify load plant info

--- coursework/block/one/model/api.php
+++ coursework/block/one/model/api.php
@@ -19,6 +19,34 @@ function getAllPlants()
     return json_encode($rows, JSON_INVALID_UTF8_IGNORE);
 }
 
+function getPlant($id){
+    global $conn;
+
+    $sql = "SELECT * FROM CMP306BlockOnePlants WHERE id = $id" ;
+
+
+    $res = mysqli_query($conn, $sql)->fetch_assoc();
+
+    $plant_id = $res["id"];
+    $scientific_name = $res["scientific_name"];
+    $common_name = $res["common_name"];
+    $keep_location = $res["keep_location"];
+    $description = $res["description"];
+    $image = $res["image"];
+
+
+    $data = array(
+        'id' => $plant_id,
+        'scientific_name' => $scientific_name,
+        'common_name' => $common_name,
+        'keep_location' => $keep_location,
+        'description' => $description,
+        'image' => $image
+    );
+
+    return json_encode($data,JSON_INVALID_UTF8_IGNORE);
+}
+
 function deletePlant($id)
 {
     global $conn;
@@ -38,7 +66,6 @@ function deletePlant($id)
 
 function editPlant($id,$description){
     global $conn;
-
     $sql = "UPDATE CMP306BlockOnePlants SET description = '$description' WHERE id = $id";
 
     if (mysqli_query($conn,$sql)){
