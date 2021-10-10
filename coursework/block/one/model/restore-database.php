97dc77042dd96a4e6b3eba04c062c79b2983505d
Ben Fleuty <bfleuty@outlook.com>
2021-10-10 22:08:46 +0100

    MVCify restore database

--- coursework/block/one/model/restore-database.php
+++ coursework/block/one/model/restore-database.php
@@ -1,29 +1,5 @@
 <?php
 
-require_once "connection.php";
-global $conn;
+require_once "api.php";
 
-$clearTableQuery = "DELETE FROM CMP306BlockOnePlants";
-$fillTableQuery = "INSERT INTO CMP306BlockOnePlants SELECT * FROM cmp306week1plants";
-
-$sql = $clearTableQuery;
-$data = array();
-
-if (mysqli_query($conn, $sql)) {
-    // good delete
-    $sql = $fillTableQuery;
-    $res = array('drop_status' => 'success');
-} else {
-    // delete fail
-    $res = array('drop_status' => 'fail');
-}
-
-$data = array_merge($data, $res);
-
-if ($sql == $fillTableQuery && mysqli_query($conn, $sql)) {
-    // good insert
-    $res = array('fill_status' => 'success');
-    $data = array_merge($data, $res);
-}
-$data = json_encode($data);
-echo $data;
+echo restoreDatabase();
