d30c51c59e5cde3a067b77bd92caf4cc17937e85
= <=>
2021-10-10 21:34:01 +0100

    MVCify delete function

--- coursework/block/one/model/api.php
+++ coursework/block/one/model/api.php
@@ -5,7 +5,6 @@ header('Content-Type: text/html; charset=utf-8');
 include("../model/connection.php");
 
 
-
 //  function to get all the items
 function getAllPlants()
 {
@@ -17,7 +16,23 @@ function getAllPlants()
     while ($r = mysqli_fetch_assoc($result)) {
         $rows[] = $r;
     }
-    return json_encode($rows,JSON_INVALID_UTF8_IGNORE);
+    return json_encode($rows, JSON_INVALID_UTF8_IGNORE);
 }
 
-?>
+function deletePlant($id)
+{
+    global $conn;
+
+    $sql = "DELETE FROM CMP306BlockOnePlants WHERE id=$id";
+
+    if (mysqli_query($conn, $sql)) {
+        $data = array('status' => 'success');
+    } else {
+        $data = array('status' => 'error');
+    }
+
+    $data = array_merge($data, array('plant_id' => $id));
+
+    return json_encode($data);
+}
+
