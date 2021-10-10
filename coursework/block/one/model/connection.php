7e61caee0d0d13645675034c813cd49ee88eec2f
Ben Fleuty <bfleuty@outlook.com>
2021-10-10 21:34:01 +0100

    MVCify delete function

--- coursework/block/one/model/connection.php
+++ coursework/block/one/model/connection.php
@@ -1,13 +1,12 @@
 <?php
-
-
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
-
 
 //  Database connections
 $servername = "lochnagar.abertay.ac.uk";
 $username = "sql1900040";
 $password = "EVUrsMKYpvIn";
 $dbname = $username;
+
+/* @var mysqli_result $conn */
 $conn = mysqli_connect($servername, $username, $password, $dbname);
