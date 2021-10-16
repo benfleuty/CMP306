088a677d9b5a12e0a5a20de697f04a3dc6a5793c
Ben Fleuty <bfleuty@outlook.com>
2021-10-16 20:34:01 +0100

    Fix textarea replacement
    Tidy buttons logic

--- coursework/block/one/controller/edit-plant-modal.js
+++ coursework/block/one/controller/edit-plant-modal.js
@@ -3,14 +3,14 @@ $(function () {
     var plantModal = $("#plantModal");
     plantModal.on("click", "#btnEditPlant", function () {
         // Store modal for access
-        var modal = $("#plantModal .modal-body");
+        var modalDescription = $("#plantModal .modal-body > .description");
         // Get text to edit
-        let text = modal.html();
+        let text = modalDescription.html();
         // store backup for reset
         og_text = text;
         // Replace text with textarea and text
         let tb = "<textarea id=\"plantDescription\" style=\"width: 100%;min-height: 150px\">" + text + "</textarea>";
-        modal.html(tb);
+        modalDescription.html(tb);
 
         // Replace edit button with save
         let saveBtn = '<button type="button" id="btnSavePlant" class="btn btn-success">Save</button>';
