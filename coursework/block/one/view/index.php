5010f3632ec50af4d12d7f0a5c97d91bc418d129
= <=>
2021-10-17 10:33:04 +0100

    Create modules & link to index.php

--- coursework/block/one/view/index.php
+++ coursework/block/one/view/index.php
@@ -4,26 +4,6 @@ session_start();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Block One | PLANTS!</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
-    <link rel="stylesheet" href="/~1900040/cmp306/assets/css/Footer-Dark.css">
-    <link rel="stylesheet" href="/~1900040/cmp306/assets/css/Header-Blue.css">
-    <link rel="stylesheet" href="/~1900040/cmp306/assets/css/Header-Dark.css">
-    <link rel="stylesheet" href="/~1900040/cmp306/assets/css/Navigation-Clean.css">
-    <link rel="stylesheet" href="/~1900040/cmp306/assets/css/Projects-Horizontal.css">
-    <link rel="stylesheet" href="/~1900040/cmp306/assets/css/styles.css">
-    <link rel="stylesheet" href="/~1900040/cmp306/assets/css/Team-Boxed.css">
-    <link rel="stylesheet" href="content/css/plants.css">
-</head>
 
 <body>
 <style>
@@ -37,33 +17,14 @@ session_start();
     }
 </style>
 
-<header class="header-blue">
-    <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
-        <div class="container-fluid"><a class="navbar-brand" href="/~1900040/cmp306/"><strong>Ben Fleuty |&nbsp;CMP
-                    306</strong><br><strong>Dynamic Web Development</strong><br></a></div>
-    </nav>
-    <div class="container hero">
-        <div class="row">
-            <div class="col-12 col-lg-6 col-xl-5 offset-xl-1">
-                <h1>Meet my plants</h1>
-                <p>Help... I keep spending money</p>
-                <button id="btnRestoreDatabase" class="btn btn-light btn-lg action-button" type="button">Restore
-                    database
-                </button>
-            </div>
-            <div class="col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none d-lg-block phone-holder">
-                <div class="phone-mockup"><img class="device" src="../../../../assets/img/plants/header.png"></div>
-            </div>
-        </div>
-    </div>
-</header>
+<?php include_once "../view/content/modules/html-header.html" ?>
+
 <section class="team-boxed">
     <div class="container">
         <div class="intro">
             <h2 class="text-center">Plants</h2>
             <p class="text-center">Meet my plants</p>
-            <p class="text-center">Click the restore database above to ensure you are seeing the original, unedited
-                data.</p>
+            <p class="text-center">Click the restore database above to ensure you are seeing the original, unedited data.</p>
         </div>
 
         <!-- Display items -->
@@ -167,27 +128,11 @@ session_start();
 <!-- New Plant Modal END-->
 
 
-<footer class="footer-dark">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 item text">
-                <h3>Ben Fleuty | 1900040</h3>
-                <p>This website is for educational purposes only. Content may be subject to copyright.</p>
-            </div>
-        </div>
-        <p class="copyright">Ben Fleuty and whoever owns the stuff I'm... borrowing © 2021</p>
-    </div>
-</footer>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"</script>
 <script src="../controller/populate-modal.js"></script>
 <script src="../controller/delete-plant-modal.js"></script>
 <script src="../controller/update-plant.js"></script>
