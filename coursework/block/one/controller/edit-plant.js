97b43e276194633a3d471e95d0551708602a481e
= <=>
2021-10-17 09:18:58 +0100

    Fix textarea replacing other content

--- coursework/block/one/controller/edit-plant.js
+++ coursework/block/one/controller/edit-plant.js
@@ -12,15 +12,12 @@ $(function () {
                     return;
                 }
                 console.log("Description updated");
-
                 $("#plantModal .modal-body > .description").html(text);
-
-
-
                 let deleteButton = '<button type="button" id="btnDeletePlant" class="btn btn-danger modal-button">Delete</button>';
                 let editButton = '<button type="button" id="btnEditPlant" class="btn btn-warning modal-button">Edit</button>';
                 let content = deleteButton + editButton;
                 $("#plantModal .modal-footer").html(content);
+
             }
         })
     })
