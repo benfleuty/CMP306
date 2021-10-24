8c7ca766c1d61adcd72eb529c347b08eb44a38c4
Ben Fleuty <bfleuty@outlook.com>
2021-10-24 15:41:06 +0100

    Add header

---
+++ coursework/block/two/view/content/modules/header.php
@@ -0,0 +1,26 @@
+<?php ?>
+
+<header>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <a class="navbar-brand" href="#">B&Cue</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId"
+                aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+            <form class="form-inline m-2 my-lg-0">
+                <button class="btn btn-info m-1 my-sm-0" type="submit">Login</button>
+                <button class="btn btn-outline-info m-1 my-sm-0" type="submit">Sign Up</button>
+            </form>
+        </div>
+    </nav>
+</header>
