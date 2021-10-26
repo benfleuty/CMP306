ec3c8d04c5551f3b9ba17aca157b7517c0790807
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-26 18:54:07 +0100

    Change to table layout

--- coursework/block/two/view/admin/admin-products-options.php
+++ coursework/block/two/view/admin/admin-products-options.php
@@ -1,30 +1,17 @@
 <?php ?>
-<script src="admin/admin-products-options.js"></script>
+<!--<script src="admin/admin-products-options.js"></script>-->
 <div class="container text-center">
-    <div class="row row-cols-2">
-        <div class="col-md-12 mb-2">
-            <div class="card admin-landing-option admin-landing-option-view">
-                <div id="viewAllProducts" class="card-body">
-                    <h3 class="card-title">View all products</h3>
-                    <p class="card-text">Select this option to view all products in the database.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card admin-landing-option admin-landing-option-edit">
-                <div id="editProduct" class="card-body">
-                    <h3 class="card-title">Edit a product</h3>
-                    <p class="card-text">Select this option to edit a product in the database.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card admin-landing-option admin-landing-option-delete">
-                <div id="deleteProduct" class="card-body">
-                    <h3 class="card-title">Delete a product</h3>
-                    <p class="card-text">Select this option to delete a product from the database.</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
 </div>
