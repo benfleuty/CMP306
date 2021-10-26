7e1053405517b91e852372d7874ae1a1427c1aae
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-26 08:56:58 +0100

    Add user's name to header when signed in

--- coursework/block/two/view/content/modules/signinUI.php
+++ coursework/block/two/view/content/modules/signinUI.php
@@ -1,6 +1,7 @@
 <?php ?>
 <form class="form-inline m-2 my-lg-0">
     <?php if (isset($_SESSION["user_id"])) : ?>
+    <span class="user-welcome mx-2"><?= getUserById($_SESSION["user_id"])["username"] ?></span>
         <button class="btn btn-info m-1 my-sm-0 btn-basket">Basket</button>
         <button class="btn btn-link my-sm-0 btn-logout">Sign Out</button>
     <?php else: ?>
