bcec22dd4175ec66a54c7db5d31f5a4f4d01e6a1
Ben Fleuty <bfleuty@outlook.com>
2021-10-26 07:36:40 +0100

    Add product preview template

--- coursework/block/two/view/index.php
+++ coursework/block/two/view/index.php
@@ -16,12 +16,29 @@ include_once "../model/api.php";
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="./content/css/header.css">
+    <link rel="stylesheet" href="./content/css/index.css">
 </head>
 <body>
 <?php if (isset($_SESSION["user_id"])) {
     echo "Welcome " . getUserById($_SESSION["user_id"])["username"];
 } ?>
 <?php include_once __DIR__ . "/content/modules/header.php"; ?>
+
+<div class="content mx-auto my-3">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">$productname$</h3>
+                    <img src="https://via.placeholder.com/200?text=Product+Image" alt="Image of a product" class="img-fluid rounded">
+                    <p class="card-text">$price$</p>
+                    <p class="card-text text-start">$description$</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 </body>
 </html>
