f2ca66023c586aa27eed910cece9f9755a0d8a10
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-26 14:06:41 +0100

    Remove unnecessary redirections

--- coursework/block/two/index.php
+++ coursework/block/two/index.php
@@ -1,24 +1,4 @@
 <?php
 session_start();
 
-$header_location = "Location: view/index.php";
-
-class redirect_locations
-{
-    const employee_index = "employee_index";
-}
-
-if (isset($_SESSION["redirect_from"])) {
-    switch($_SESSION["redirect_from"])
-    {
-        case redirect_locations::employee_index:
-            $header_location = "Location: view/employeeSignIn.php";
-            break;
-
-        default:
-            $header_location = "Location: view/index.php";
-            break;
-    }
-}
-
-header($header_location);
+header("Location: view/index.php");
