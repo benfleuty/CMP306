3dd01bb2261a8b058ae5b463c9ae6414c077517b
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-26 18:26:49 +0100

    Add styling

--- coursework/block/two/view/admin/admin-products-options.php
+++ coursework/block/two/view/admin/admin-products-options.php
@@ -2,8 +2,8 @@
 <script src="admin/admin-products-options.js"></script>
 <div class="container text-center">
     <div class="row row-cols-2">
-        <div class="col-md-6">
-            <div class="card admin-landing-option">
+        <div class="col-md-12 mb-2">
+            <div class="card admin-landing-option admin-landing-option-view">
                 <div id="viewAllProducts" class="card-body">
                     <h3 class="card-title">View all products</h3>
                     <p class="card-text">Select this option to view all products in the database.</p>
@@ -11,15 +11,7 @@
             </div>
         </div>
         <div class="col-md-6">
-            <div class="card admin-landing-option">
-                <div id="viewProduct" class="card-body">
-                    <h3 class="card-title">View a specific product</h3>
-                    <p class="card-text">Select this option to view a product in the database.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card admin-landing-option">
+            <div class="card admin-landing-option admin-landing-option-edit">
                 <div id="editProduct" class="card-body">
                     <h3 class="card-title">Edit a product</h3>
                     <p class="card-text">Select this option to edit a product in the database.</p>
@@ -27,7 +19,7 @@
             </div>
         </div>
         <div class="col-md-6">
-            <div class="card admin-landing-option">
+            <div class="card admin-landing-option admin-landing-option-delete">
                 <div id="deleteProduct" class="card-body">
                     <h3 class="card-title">Delete a product</h3>
                     <p class="card-text">Select this option to delete a product from the database.</p>
