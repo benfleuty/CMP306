1eab3621740ab3210eb6e3510f50893cd7712c1d
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-26 15:09:39 +0100

    Add styling to admin options

--- coursework/block/two/view/admin-products-options.php
+++ coursework/block/two/view/admin-products-options.php
@@ -3,7 +3,7 @@
 <div class="container text-center">
     <div class="row row-cols-2">
         <div class="col-md-6">
-            <div class="card">
+            <div class="card admin-landing-option">
                 <div id="viewAllProducts" class="card-body">
                     <h3 class="card-title">View all products</h3>
                     <p class="card-text">Select this option to view all products in the database.</p>
@@ -11,7 +11,7 @@
             </div>
         </div>
         <div class="col-md-6">
-            <div class="card">
+            <div class="card admin-landing-option">
                 <div id="viewProduct" class="card-body">
                     <h3 class="card-title">View a specific product</h3>
                     <p class="card-text">Select this option to view a product in the database.</p>
@@ -19,7 +19,7 @@
             </div>
         </div>
         <div class="col-md-6">
-            <div class="card">
+            <div class="card admin-landing-option">
                 <div id="editProduct" class="card-body">
                     <h3 class="card-title">Edit a product</h3>
                     <p class="card-text">Select this option to edit a product in the database.</p>
@@ -27,7 +27,7 @@
             </div>
         </div>
         <div class="col-md-6">
-            <div class="card">
+            <div class="card admin-landing-option">
                 <div id="deleteProduct" class="card-body">
                     <h3 class="card-title">Delete a product</h3>
                     <p class="card-text">Select this option to delete a product from the database.</p>
--- coursework/block/two/view/admin.php
+++ coursework/block/two/view/admin.php
@@ -14,6 +14,7 @@ include_once "../model/api.php";
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="./content/css/header.css">
     <link rel="stylesheet" href="./content/css/global-body.css">
+    <link rel="stylesheet" href="./content/css/admin.css">
 </head>
 <body>
 <?php include_once __DIR__ . "/content/modules/header.php"; ?>
