90c9423d0a887220f3cea3ec070becba535018eb
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-26 14:53:12 +0100

    Add dummy options for admin

---
+++ coursework/block/two/view/admin-products-options.php
@@ -0,0 +1,38 @@
+<?php ?>
+
+<div class="container text-center">
+    <div class="row row-cols-2">
+        <div class="col-md-6">
+            <div class="card">
+                <div id="viewAllProducts" class="card-body">
+                    <h3 class="card-title">View all products</h3>
+                    <p class="card-text">Select this option to view all products in the database.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div id="viewProduct" class="card-body">
+                    <h3 class="card-title">View a specific product</h3>
+                    <p class="card-text">Select this option to view a product in the database.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div id="editProduct" class="card-body">
+                    <h3 class="card-title">Edit a product</h3>
+                    <p class="card-text">Select this option to edit a product in the database.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div id="deleteProduct" class="card-body">
+                    <h3 class="card-title">Delete a product</h3>
+                    <p class="card-text">Select this option to delete a product from the database.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- coursework/block/two/view/admin.php
+++ coursework/block/two/view/admin.php
@@ -1,1 +1,27 @@
-<?php
+<?php session_start();
+include_once "../model/api.php";
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Admin | Block Two | 1900040</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./content/css/header.css">
+    <link rel="stylesheet" href="./content/css/global-body.css">
+</head>
+<body>
+<?php include_once __DIR__ . "/content/modules/header.php"; ?>
+
+<div class="content mx-auto my-3">
+    <?php include_once __DIR__ . "/admin-products-options.php"; ?>
+</div>
+
+<?php include_once __DIR__ . "/content/modules/footer.php"; ?>
+</body>
+</html>
