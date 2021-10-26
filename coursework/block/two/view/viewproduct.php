056d3a4872374d21ba4bf9c7921d940c7b5565fc
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-26 09:04:26 +0100

    Include global-body.css

--- coursework/block/two/view/viewproduct.php
+++ coursework/block/two/view/viewproduct.php
@@ -15,7 +15,7 @@ include_once "../model/api.php";
     <!--suppress JSUnresolvedLibraryURL -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="./content/css/header.css">
-    <link rel="stylesheet" href="./content/css/header.css">
+    <link rel="stylesheet" href="./content/css/global-body.css">
 </head>
 <body>
 <?php include_once __DIR__ . "/content/modules/header.php"; ?>
