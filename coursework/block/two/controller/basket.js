463caaa74a6e0784d04280f6253e9217827944d6
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-29 21:42:06 +0100

    Add removing item from basket

---
+++ coursework/block/two/controller/basket.js
@@ -0,0 +1,15 @@
+$(function(){
+    $("#btnRemoveFromBasket").on("click",function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: '/~1900040/cmp306/coursework/block/two/model/empty-basket.php',
+            success: function (response) {
+                window.location.reload();
+            },
+            error: function (a, b, c) {
+                console.log(c);
+            }
+        });
+    })
+})
