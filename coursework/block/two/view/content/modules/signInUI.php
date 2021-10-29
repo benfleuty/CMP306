eefc353e88b73543e8b555f6dfb7319e82029e87
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-29 21:41:29 +0100

    Show basket quantity throughout site

--- coursework/block/two/view/content/modules/signInUI.php
+++ coursework/block/two/view/content/modules/signInUI.php
@@ -1,8 +1,12 @@
 <?php ?>
 <form class="form-inline m-2 my-lg-0">
-    <?php if (isset($_SESSION["user_id"])) : ?>
+    <?php if (isset($_SESSION["user_id"])) :
+        $basket = "Basket";
+        if (isset($_SESSION["basket"]))
+            $basket .= " (1)";
+        ?>
         <span class="user-welcome mx-2"><?= getUserById($_SESSION["user_id"])["username"] ?></span>
-        <button class="btn btn-info m-1 my-sm-0 btn-basket">Basket</button>
+        <button class="btn btn-info m-1 my-sm-0 btn-basket"><?= $basket ?></button>
         <button class="btn btn-link my-sm-0 btn-signOut">Sign Out</button>
     <?php else: ?>
         <button class="btn btn-info m-1 my-sm-0 btn-signIn">Sign In</button>
