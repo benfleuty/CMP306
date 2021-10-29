eea725ec87ab04fe6c8de62f2b2cc3dc2062cd80
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-29 10:41:34 +0100

    Add frontend display of deleteproduct.php

--- coursework/block/two/view/admin/deleteproduct.php
+++ coursework/block/two/view/admin/deleteproduct.php
@@ -1,1 +1,51 @@
-<?php
+<?php session_start();
+
+include_once "/home/1900040/public_html/cmp306/coursework/block/two/model/api.php";
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Delete Product | Admin | Block Two | 1900040</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!--suppress JSUnresolvedLibraryURL -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/~1900040/cmp306/coursework/block/two/view/content/css/header.css">
+    <link rel="stylesheet" href="/~1900040/cmp306/coursework/block/two/view/content/css/global-body.css">
+</head>
+<body>
+<?php include_once "/home/1900040/public_html/cmp306/coursework/block/two/view/content/modules/header.php"; ?>
+
+<div class="content mx-auto my-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <img src="https://via.placeholder.com/1280x720" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-7">
+                <div class="product-title ">
+                    <h2>$productTitle$</h2>
+                </div>
+                <div class="product-price ">
+                    <span class="text-muted">$productPrice$</span>
+                </div>
+                <div class="product-description mt-1">
+                    <span>lorem ipsum dolor sit amet.</span>
+                </div>
+            </div>
+        </div>
+        <div class="delete-container my-2">
+            <form class="w-100 bg-black">
+                <button id="btnDeleteProduct" class="btn btn-danger w-100">Delete this product</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include_once "/home/1900040/public_html/cmp306/coursework/block/two/view/content/modules/footer.php"; ?>
+</body>
+</html>
