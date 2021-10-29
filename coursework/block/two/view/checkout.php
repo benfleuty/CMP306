482568f2179be7f7b51e85881c649bf00340b19d
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-29 22:15:57 +0100

    Add checkout UI

--- coursework/block/two/view/checkout.php
+++ coursework/block/two/view/checkout.php
@@ -18,7 +18,7 @@ require_once "/home/1900040/public_html/cmp306/coursework/block/two/model/api.ph
 </head>
 <body>
 <?php include_once "/home/1900040/public_html/cmp306/coursework/block/two/view/content/modules/header.php"; ?>
-<script src="/~1900040/cmp306/coursework/block/two/controller/basket.js"></script>
+<script src="/~1900040/cmp306/coursework/block/two/controller/checkout.js"></script>
 <div class="content mx-auto my-3">
     <div>
         <?php
@@ -30,7 +30,28 @@ require_once "/home/1900040/public_html/cmp306/coursework/block/two/model/api.ph
         ?>
         <h1>Checkout</h1>
         <p>Item: <?= $product["name"] ?></p>
-        <p>Total: £<?= number_format($product["name"], 2) ?></p>
+        <p>Total: £<?= number_format($product["price"], 2) ?></p>
+        <p>Payments handled by Aberpay</p>
+        <form>
+            <div class="mb-3">
+                <label for="txtCardNumber"> Card number
+                    <input id="txtCardNumber" type="number" class="form-control" min="0" minlength="16" maxlength="16" placeholder="XXXX XXXX XXXX XXXX">
+                </label>
+            </div>
+            <div class="mb-3">
+                <label for="txtSortCode"> Sort code
+                    <input id="txtSortCode" type="number" class="form-control" min="0" minlength="6" maxlength="6" placeholder="XX-XX-XX">
+                </label>
+            </div>
+            <div class="mb-3">
+                <label for="txtCVC" > CVC
+                    <input id="txtCVC" type="number" class="form-control" min="0" minlength="3" maxlength="3" placeholder="XXX">
+                </label>
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary" id="btnConfirmCheckout">Complete transaction</button>
+            </div>
+        </form>
     </div>
 </div>
 
