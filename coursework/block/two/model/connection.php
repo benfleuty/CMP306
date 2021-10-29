4684d64196b430bb8fbaadd13dab301fe131e266
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-29 13:46:11 +0100

    Remove error_logging.php include

--- coursework/block/two/model/connection.php
+++ coursework/block/two/model/connection.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once "/home/1900040/public_html/cmp306/coursework/block/two/model/config.php";
-
 //  Database connections
 $servername = "lochnagar.abertay.ac.uk";
 $username = "sql1900040";
