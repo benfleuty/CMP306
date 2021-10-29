5230182cc80db315d1e72d8af0cb9ba0dbc27f26
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-29 17:41:17 +0100

    Add restore-database to footer

--- coursework/block/two/view/content/modules/footer.php
+++ coursework/block/two/view/content/modules/footer.php
@@ -6,6 +6,8 @@
                 <h3>Ben Fleuty | 1900040</h3>
                 <p>This website is for educational purposes only. Content may be subject to copyright.</p>
                 <p>Ben Fleuty and whoever owns the stuff I'm... borrowing © 2021</p>
+                <script src="/~1900040/cmp306/coursework/block/two/view/content/modules/restore-database.js"></script>
+                <button class="btn btn-danger" id="btnResetProducts">Restore Product Database</button>
             </div>
         </div>
     </div>
