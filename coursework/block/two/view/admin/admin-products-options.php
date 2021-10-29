675d5821db390bfa640ca35c544909f4867d99c3
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-29 14:17:54 +0100

    Display all products

--- coursework/block/two/view/admin/admin-products-options.php
+++ coursework/block/two/view/admin/admin-products-options.php
@@ -1,7 +1,11 @@
-<?php session_start(); ?>
+<?php session_start();
+require_once "/home/1900040/public_html/cmp306/coursework/block/two/model/api.php";
+require_once "/home/1900040/public_html/cmp306/coursework/block/two/model/productTableRow.php";
+
+?>
 <!--<script src="admin/admin-products-options.js"></script>-->
-<div class="container text-center">
-    <table class="table">
+<div class="text-center">
+    <table class="table table-hover table-sm">
         <thead>
         <tr>
             <th>Name</th>
@@ -11,7 +15,44 @@
         </tr>
         </thead>
         <tbody>
+        <?php
 
+        $products = getAllProducts();
+
+        foreach ($products["products"] as $product) {
+            $output = "<tr>";
+            $output .= "<td>" . $product["name"] . "</td>";
+
+            $price = $product["price"];
+            $price = number_format($price,2);
+
+            $output .= "<td>" . $price . "</td>";
+
+
+            $desc = $product["description"];
+
+            if (count_chars($desc) > 100) {
+                $desc = substr($desc, 0, 100);
+                $desc = trim($desc);
+                $desc .= "...";
+            }
+
+            $output .= "<td style='text-align: left'>" . $desc . "</td>";
+
+            $btnView = '<button class="btn btn-primary btn-view">View<span class="id d-none">' . $product["id"] . '</span></button>';
+            $btnEdit = '    <button class="btn btn-warning btn-edit">Edit<span class="id d-none">' . $product["id"] . '</span></button>';
+            $btnDelete = '<button class="btn btn-danger btn-delete">Delete<span class="id d-none">' . $product["id"] . '</span></button>';
+
+            $buttons = $btnView . $btnEdit . $btnDelete;
+
+            $output .= "<td>" . $buttons . "</td>";
+
+            $output .= "</tr>";
+
+            echo $output;
+        }
+
+        ?>
         </tbody>
     </table>
 </div>
