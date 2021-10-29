8a603a6d5c877954e88e233c5269e207b1480139
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-29 15:15:09 +0100

    Add functionality to table buttons

--- coursework/block/two/view/admin/admin-products-options.js
+++ coursework/block/two/view/admin/admin-products-options.js
@@ -1,19 +1,33 @@
 $(function () {
+    let action = null;
+    let selected_product_id = -1;
 
-    $("#viewAllProducts").on("click",function () {
-        window.location.href = "/~1900040/cmp306/coursework/block/two/view/admin/viewproducts.php";
-    });
+    $("button").on("click", function (e) {
+        e.preventDefault();
+    })
 
-    $("#viewProduct").on("click",function () {
-        window.location.href = "/~1900040/cmp306/coursework/block/two/view/admin/viewproduct.php";
-    });
+    $(".btn-data-action").on("click", function (e) {
+        e.preventDefault();
+        let vals = e.target.name.split('-');
+        let action = vals[0];
+        selected_product_id = vals[1];
 
-    $("#editProduct").on("click",function () {
-        window.location.href = "/~1900040/cmp306/coursework/block/two/view/admin/editproduct.php";
-    });
+        do_action(action);
+    })
 
-    $("#deleteProduct").on("click",function () {
-        window.location.href = "/~1900040/cmp306/coursework/block/two/view/admin/deleteproduct.php";
-    });
+    function do_action(action) {
+        switch (action) {
+            case "view":
+                window.location.href = "/~1900040/cmp306/coursework/block/two/view/admin/viewproduct.php?id=" + selected_product_id;
+                break;
+            case "edit":
+                window.location.href = "/~1900040/cmp306/coursework/block/two/view/admin/editproduct.php?id=" + selected_product_id;
+                break;
+            case "delete":
+                window.location.href = "/~1900040/cmp306/coursework/block/two/view/admin/deleteproduct.php?id=" + selected_product_id;
+                break;
 
-})
+            default:console.error("error doing action");
+        }
+    }
+});
