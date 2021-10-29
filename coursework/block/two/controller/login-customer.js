79f2f7426a1cd0d3363f032adbe6623c12654a4f
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-29 21:32:11 +0100

    Add link to register from login.php

--- coursework/block/two/controller/login-customer.js
+++ coursework/block/two/controller/login-customer.js
@@ -39,4 +39,9 @@ $(function () {
             }
         })
     })
+
+    $('[name="signUp"]').on("click", function (e) {
+        e.preventDefault();
+        window.location.href = "/~1900040/cmp306/coursework/block/two/view/register.php";
+    });
 })
