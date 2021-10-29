78f1d05acd7fe202a2baae35d0905f7a94f135af
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-10-29 18:27:30 +0100

    Load products into the homepage

--- coursework/block/two/view/index.php
+++ coursework/block/two/view/index.php
@@ -1,5 +1,5 @@
 <?php session_start();
-include_once "../model/api.php";
+require_once "/home/1900040/public_html/cmp306/coursework/block/two/model/api.php";
 ?>
 
 <!doctype html>
@@ -19,17 +19,46 @@ include_once "../model/api.php";
 <?php include_once "/home/1900040/public_html/cmp306/coursework/block/two/view/content/modules/header.php"; ?>
 <div class="content mx-auto my-3">
     <div class="row">
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h3 class="card-title">$productname$</h3>
-                    <img src="https://via.placeholder.com/200?text=Product+Image" alt="Image of a product"
-                         class="img-fluid rounded">
-                    <p class="card-text">$price$</p>
-                    <p class="card-text text-start">$description$</p>
-                </div>
-            </div>
-        </div>
+        <?php
+
+        $products = getAllProducts();
+
+        foreach ($products["products"] as $product) {
+            $defaultImg = $product["image"] === "https://via.placeholder.com/300";
+
+            $imgBasePath = "";
+
+            if (!$defaultImg) {
+                $imgBasePath = "/~1900040/cmp306/coursework/block/two/img/";
+            }
+
+            foreach ($product as $key => $value) {
+                $product[$key] = htmlspecialchars($value);
+            }
+
+            $output = '<div class="col-md-4"><div class="card"><div class="card-body">';
+            $output .= '<h3 class="card-title">' . $product["name"] . '</h3>';
+            $output .= '<img src="' . $imgBasePath . $product["image"] . '" alt="Image of a product" class="img-fluid rounded" style="max-height:200px">';
+
+            $price = $product["price"];
+            $price = number_format($price, 2);
+            $output .= '<p class="card-text">' . $price . '</p>';
+
+            $desc = $product["description"];
+
+            if (count_chars($desc) > 100) {
+                $desc = substr($desc, 0, 100);
+                $desc = trim($desc);
+                $desc .= "...";
+            }
+            $output .= '<p class="card-text text-start" >' . $desc . '</p>';
+
+            $output .= '<form><button class="btn btn-outline-primary btn-view-product w-100" id="view-'. $product["id"] .'">View</button></form>';
+
+            $output .= "</div></div></div>";
+            echo $output;
+        }
+        ?>
     </div>
 </div>
 
