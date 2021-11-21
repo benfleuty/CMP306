5bbef4c12657ef7db5d88498dd755eb4bf7218e4
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-11-21 15:03:17 +0000

    Parse message from imp and respond to imp

--- coursework/block/three/controller/impresponse.php
+++ coursework/block/three/controller/impresponse.php
@@ -2,4 +2,19 @@
 
 $message = file_get_contents('php://input');
 
-echo "received";
+$message = json_decode($message, true)[0];
+$output = "";
+
+if ($message['pin'] === '5') {
+    $output .= 'red state set to';
+}elseif ($message['pin'] === '7') {
+    $output .= 'green state set to';
+}elseif ($message['pin'] === '8') {
+    $output .= 'internal temp is ';
+}elseif ($message['pin'] === '9') {
+    $output .= 'external temp is';
+}
+
+$output .= $message["value"];
+
+echo $output;
