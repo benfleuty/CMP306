073a977b3528169843680cd224ea5169702ba6b1
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2021-12-16 22:42:28 +0000

    update to relevant project

--- ws/modules/xml-helper.php
+++ ws/modules/xml-helper.php
@@ -2,11 +2,12 @@
 //  convert a record from database to XML
 function convert_to_xml($query)
 {
-    $txt = '<employee>';
+    $txt = '<article>';
     $txt = $txt . '<id>' . $query['id'] . '</id>';
-    $txt = $txt . '<employee_name>' . $query['employee_name'] . '</employee_name>';
-    $txt = $txt . '<employee_age>' . $query['employee_age'] . '</employee_age>';
-    $txt = $txt . '<employee_salary>' . $query['employee_salary'] . '</employee_salary>';
-    $txt = $txt . '</employee>';
+    $txt = $txt . '<title>' . $query['title'] . '</title>';
+    $txt = $txt . '<description>' . $query['description'] . '</description>';
+    $txt = $txt . '<image>' . $query['image'] . '</image>';
+    $txt = $txt . '<link>' . $query['link'] . '</link>';
+    $txt = $txt . '</article>';
     return $txt;
 }
